319ac2c50b8257397843b0a2810ae7769f312625
Jannis Schreiber <info@lietzau-consulting.de>
2022-03-26 11:33:41 +0100

    initial project setup & boilerplate cleanup

---
+++ scripts/shell/setup.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+set -e
+
+alias='JsLwcUtils'
+duration=7
+configFile='config/default-scratch-def.json'
+devhubusername=
+
+while getopts a:d:f:v: option
+do
+    case "${option}" in
+        a )             alias=${OPTARG};;
+        d )             duration=${OPTARG};;
+        f )             configFile=${OPTARG};;
+        v )             devhubusername=${OPTARG};;
+        * )
+    esac
+done
+
+echo "npm ci"
+npm ci
+
+if [ -z "$devhubusername" ]; then
+    echo "sfdx force:org:create -d $duration -f $configFile -a $alias -s"
+    sfdx force:org:create -d $duration -f $configFile -a $alias -s
+else
+    echo "sfdx force:org:create -v $devhubusername -d $duration -f $configFile -a $alias -s"
+    sfdx force:org:create -v $devhubusername -d $duration -f $configFile -a $alias -s
+fi
+
+echo "sfdx force:source:push -u $alias"
+sfdx force:source:push -u $alias
+
+echo "sfdx force:data:tree:import -p data/plans/standard-plan.json -u $alias"
+sfdx force:data:tree:import -p data/plans/standard-plan.json -u $alias
+
+echo "sfdx force:org:open -u $alias"
+sfdx force:org:open -u $alias
